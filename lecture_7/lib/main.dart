9bc14bac2f2ef2435af8a4a3bd487e4ebd041c40
AlexxnderD <velzplay@gmail.com>
2024-12-26 16:25:00 +0300

    FirstCommit

---
+++ lecture_7/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:lecture_7/focus.dart';
+import 'package:lecture_7/form.dart';
+import 'package:lecture_7/gesture.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    // home: GesturePage(),
+    home: FormPage(),
+  ));
+}
